6560b5c41cff0c3cc713b69c369f08e268049a93
DtlipKbf <lizabeta.li.p@gmail.com>
2023-05-29 19:46:29 +0300

    WELL DONE

--- lesson69/GasStation.cpp
+++ lesson69/GasStation.cpp
@@ -3,7 +3,7 @@
 int GasStation::calculateTotalGas(Transport*transport, int size) {
 	int total = 0;
 	for (int i = 0; i < size; i++) {
-		total += 0;
+		total += transport[i].getTank();
 	}
 	return total;
 };
