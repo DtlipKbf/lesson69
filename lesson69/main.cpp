12d1d3bb901aedf40663f277e293fb0fc12d8403
DtlipKbf <lizabeta.li.p@gmail.com>
2023-05-30 18:43:39 +0300

    ///

--- lesson69/main.cpp
+++ lesson69/main.cpp
@@ -5,6 +5,7 @@ int main() {
 	Car car1(50, 200, 1600);
 	Car car2(60, 240, 2000);
 	Car car3(40, 160, 1200);
+	// Car *car2 = new Car();
 
 	Bus bus(200, 50);
 
@@ -13,6 +14,9 @@ int main() {
 
 	Transport transport[15] = {car1,car2,car3, bus, truck1, truck2};
 	int size = Transport::getCount();
+
+	//Transport* transport = new Transport
+	// transport = new Car()/Bus()/Truck();
 
 	int total = GasStation::calculateTotalGas(transport, size);
 
